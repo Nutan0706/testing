e96f8c7cd446e79eeceb7ba9f01db9ee35b5bbca
Nutan patel <nutan.patel@Geminisolutions.com>
2024-12-14 19:46:38 +0530

    buy sell problem

---
+++ buy_sell.py
@@ -0,0 +1,13 @@
+def max_profit(prices):
+    n = len(prices)
+    res = 0
+
+    for i in range(n-1):
+        for j in range(i+1,n):
+            res = max(res,prices[j]- prices[i])
+
+    return res
+
+if __name__ == "__main__":
+    prices = [7,10,1,3,6,9,2]
+    print(max_profit(prices))
